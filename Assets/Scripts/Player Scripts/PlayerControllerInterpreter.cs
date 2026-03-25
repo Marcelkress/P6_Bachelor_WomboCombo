5727a539b9f4f443dfbdb094fa091a874a32c9b6
Marcelkress <marcel@kress.dk>
2026-03-25 14:58:41 +0100

    nu gider jeg ikke mere

--- Assets/Scripts/Player Scripts/PlayerControllerInterpreter.cs
+++ Assets/Scripts/Player Scripts/PlayerControllerInterpreter.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerControllerInterpreter : MonoBehaviour
 {
     private PlayerComboInput comboInput;
+    public InputReceiver inputReceiver;
 
     public int controllerValue;
     
@@ -16,6 +17,9 @@ public class PlayerControllerInterpreter : MonoBehaviour
     
     [Header("Star interval")] public int starMin;
     public int starMax;
+    
+    private static bool playerOneTaken;
+    private InputStruct input;
     
     void Start()
     {
@@ -31,9 +35,13 @@ public class PlayerControllerInterpreter : MonoBehaviour
         {
             Debug.Log("SUN!");
         }
-        else if()
+        else if (val < moonMax && val > moonMin)
         {
-            
+            Debug.Log("MOON!");
+        }
+        else if (val < starMax && val > starMin)
+        {
+            Debug.Log("STAR!");
         }
     }
 }
