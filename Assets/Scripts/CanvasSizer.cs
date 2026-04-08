637c104658b8feab410020c6202c1218ee51e5f1
JacobVigsoe <eh66xs@student.aau.dk>
2026-04-08 10:39:22 +0200

    hallo

--- Assets/Scripts/CanvasSizer.cs
+++ Assets/Scripts/CanvasSizer.cs
@@ -6,15 +6,29 @@ public class CanvasSizer : MonoBehaviour
     public float valueToAdd = 450;
     private Enemy enemyScript;
     public bool useBossLogic = false, useProjectileLogic = false;
+    public int scalingFactor = 2;
     private BossComboSystem bossComboSystem;
     private EnemyProjectile enemyProjectile;
     private Canvas canvas;
 
     private int comboSize;
+
+    private Vector3 currentCanvasSize;
+    private Vector3 targetCanvasSize;
+
+    private float initialCanvasDistance;
+
+    private GameObject player;
         
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        canvas = GetComponent<Canvas>();
+        currentCanvasSize = canvas.transform.localScale;
+
+        initialCanvasDistance = Vector3.Distance(player.transform.position, transform.position);
+        targetCanvasSize = currentCanvasSize/scalingFactor;
         if (useBossLogic)
         {
             bossComboSystem = GetComponentInParent<BossComboSystem>();
@@ -36,11 +50,23 @@ public class CanvasSizer : MonoBehaviour
         
         for (int i = 1; i < comboSize; i++)
         {
-            size += 450;
+            size += valueToAdd;
         }
 
         canvas.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size); 
     }
 
+    // Sizes the canvas based on the distance to the player, making it smaller as the player gets closer
+    private void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        float dis = Vector3.Distance(playerPos, transform.position);
+
+        float clampedDistance = Mathf.Clamp01(dis / initialCanvasDistance);
+        
+        Vector3 newCurrentCanvasSize = Vector3.Lerp(targetCanvasSize, currentCanvasSize, clampedDistance);
+        canvas.transform.localScale = newCurrentCanvasSize;
+    }
     
 }
