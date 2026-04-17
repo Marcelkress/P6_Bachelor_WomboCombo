821800b5460bf4cb565764dd581158e6cc77777d
JacobVigsoe <eh66xs@student.aau.dk>
2026-04-17 10:45:07 +0200

    End credit demo canvas added

--- Assets/Scripts/CastleDoorTrigger.cs
+++ Assets/Scripts/CastleDoorTrigger.cs
@@ -6,9 +6,22 @@ public class CastleDoorTrigger : MonoBehaviour
     [SerializeField] private Transform doorTransformPivot;
     [SerializeField] private float speed = 1f;
     [SerializeField] private Vector3 openRotation = new Vector3(0, 90, 0);
+    public CanvasGroup EndOfGameCanvasGroup;
+    public bool endGameWhenTriggered = false;
+    void Start()
+    {
+
+        EndOfGameCanvasGroup.alpha = 0;
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (endGameWhenTriggered && other.CompareTag("Player"))
+        {
+            EndOfGameCanvasGroup.DOFade(1, 2f).OnComplete(() => Time.timeScale = 0);
+            return;
+        }
+        if (other.CompareTag("Player") && !endGameWhenTriggered)
         {
             Invoke(nameof(OpenDoor), 0.6f);
         }
